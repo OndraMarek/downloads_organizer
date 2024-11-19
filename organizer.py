2f82a80ef3a7e619cc44b5eb227544812f3ce9a9
OndraMarek <czondrejmarek@gmail.com>
2024-11-19 13:28:49 +0100

    Enhance file organization logic to prevent overwriting files by appending suffixes

--- organizer.py
+++ organizer.py
@@ -2,21 +2,33 @@ import os
 
 def create_folders():
     for key in file_types:
-        new_folder_path = os.path.join(folder_path,key)
+        new_folder_path = os.path.join(folder_path, key)
         os.makedirs(new_folder_path, exist_ok=True)
-    others_folder_path = os.path.join(folder_path,"OTHERS")
+    others_folder_path = os.path.join(folder_path, "OTHERS")
     os.makedirs(others_folder_path, exist_ok=True)
-    
+
 def organize_files_to_folders():
     for file in os.listdir(folder_path):
         moved = False
         file_name, file_ext = os.path.splitext(file)
         for key in file_types:
             if file_ext in file_types[key] and file_name not in file_types.keys() and file_name != "OTHERS":
-                os.rename(os.path.join(folder_path,file), os.path.join(folder_path,key,file))
+                dest_path = os.path.join(folder_path, key, file)
+                counter = 1
+                while os.path.exists(dest_path):
+                    new_file_name = f"{file_name}_new{counter}{file_ext}"
+                    dest_path = os.path.join(folder_path, key, new_file_name)
+                    counter += 1
+                os.rename(os.path.join(folder_path, file), dest_path)
                 moved = True
         if not moved and file_name not in file_types.keys() and file_name != "OTHERS":
-            os.rename(os.path.join(folder_path,file), os.path.join(folder_path,"OTHERS",file))
+            dest_path = os.path.join(folder_path, "OTHERS", file)
+            counter = 1
+            while os.path.exists(dest_path):
+                new_file_name = f"{file_name}_new{counter}{file_ext}"
+                dest_path = os.path.join(folder_path, "OTHERS", new_file_name)
+                counter += 1
+            os.rename(os.path.join(folder_path, file), dest_path)
 
 if __name__ == "__main__":
     folder_path = "C:/Users/ondra/Downloads"
